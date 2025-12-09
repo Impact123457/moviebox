14f3dfbc4a72c1e0e9753c5cecda220375ae86e7
Impact123457 <ema.skruba@scv.si>
2025-12-09 13:16:09 +0100

    test

--- app/(root)/watched/page.tsx
+++ app/(root)/watched/page.tsx
@@ -4,7 +4,7 @@ import { auth } from "@/auth";
 
 export default async function edit(){
     const session = await auth();//a je user prijavlen
-    const user = session?.user;
+    const user = session?.user; 
     return(
         <section className="p-5 my-5 shadow-lg w-[900px] mx-auto min-h-[500px]">
             <div>

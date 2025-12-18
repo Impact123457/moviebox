c7320d760c9e02c8172315bacf5cfee9b783478c
Impact123457 <ema.skruba@scv.si>
2025-12-18 09:22:16 +0100

    ...

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -15,20 +15,20 @@ const Profile = async ({ params }: { params: { id: string } }) => {
     return(
         <>
         <div className="md:w-[900px] w-[400px] my-5 mx-auto h-[530px] shadow-lg">
-            <div>
-                <h1 className="m-5 font-bold text-2xl">{user.username}</h1> 
-                <div className="flex p-2">
-                    <Image src={user.image || "/defaultPFP.jpg"} alt="pfp" width={100} height={100} className="w-[100px] h-[100px] rounded-full m-3 border-2 border-black object-cover" />
-                    <div>
-                        <p className="p-5 mt-3 md:w-[400px]">{user.bio}</p>
-                        {session?.user.provider == "credentials" ? 
-                        <Link href={`./editProfile/${user?._id}`} className="rounded bg-black text-white p-2 m-5">
-                            Edit profile
-                        </Link>
-                        : "If you are logged in with GitHub you cannot edit your profile!!"}
-                    </div>
-                </div>       
-            </div>
+                <div>
+                    <h1 className="m-5 font-bold text-2xl">{user.username}</h1> 
+                    <div className="flex p-2">
+                        <Image src={user.image || "/defaultPFP.jpg"} alt="pfp" width={100} height={100} className="w-[100px] h-[100px] rounded-full m-3 border-2 border-black object-cover" />
+                        <div>
+                            <p className="p-5 mt-3 md:w-[400px]">{user.bio}</p>
+                            {session?.user.provider == "credentials" ? 
+                            <Link href={`./editProfile/${user?._id}`} className="rounded bg-black text-white p-2 m-5">
+                                Edit profile
+                            </Link>
+                            : "If you are logged in with GitHub you cannot edit your profile!!"}
+                        </div>
+                    </div>       
+                </div>
             <div className="mt-5 items-center justify-center border font-bold border-black p-3 flex gap-5 bg-black text-white uppercase">
                 <Link href="/liked">Favourites</Link>
                 <Link href="/watched">Diary</Link>

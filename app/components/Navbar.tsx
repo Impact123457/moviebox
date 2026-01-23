77b3a0fd4292b42b4de1eb25fe73da4d65098702
Impact123457 <ema.skruba@scv.si>
2026-01-23 17:31:31 +0100

    `Removed unused import and restored removed code`

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
 import Image from "next/image"
-import { auth, signOut, signIn } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const Navbar = async() =>{   
     const session = await auth();//a je user prijavlen
     return(
-        <header className="bg-white shadow-sm">
+    <header className="bg-white shadow-sm">
         <nav className="flex justify-between items-center">
             <div className="p-3">
                 <Link href="/">
@@ -37,17 +37,16 @@ const Navbar = async() =>{
                     }}>
                         <button type="submit" className="uppercase font-bold cursor-pointer">Log out</button>
                     </form>
-                    <Link href={`/user/${session?.user.id}`}>
-                        <Image src="/profile.png" alt="profile icon" width={40} height={40} className="rounded-full"/>
-                    </Link>
-                    
+                        <Link href={`/user/${session?.user.id}`}>
+                            <Image src="/profile.png" alt="profile icon" width={40} height={40} className="rounded-full"/>
+                        </Link>
                     </>
                     ):(
                     <>
-                    <Link href="/login">
-                        <button type="submit" className="uppercase font-bold cursor-pointer">Log in</button>
-                    </Link>
-                </>
+                        <Link href="/login">
+                            <button type="submit" className="uppercase font-bold cursor-pointer">Log in</button>
+                        </Link>
+                    </>
                 )}
             </div>
         </nav>

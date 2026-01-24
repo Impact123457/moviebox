d584df4a9868fc0c50455e7a745b9d056cb05f0f
zuranmateo <mateo.zuran@scv.si>
2026-01-24 09:40:24 +0100

    cypress test on every commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
 });
